c0a9e9b494de6ef0b799ac35fdd77d0876c4cf86
Qianying Ruan <124208512+1239874569898@users.noreply.github.com>
2023-10-22 22:03:31 -0700

    Add files via upload

---
+++ StudentInfo.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Info() {
+    return (
+    <div>
+    <p>Qianying Ruan</p>;
+    <p>CPRG306D</p>
+    <link href = "https://github.com/1239874569898">Github</link>
+    </div>
+    );
+}
---
+++ page.js
@@ -0,0 +1,8 @@
+import StudentInfo from ".StudentInfo";
+import Link from "next/link";
+
+export default function Page() {
+  return (
+      <h1>My Shopping List</h1>   
+  );
+}
